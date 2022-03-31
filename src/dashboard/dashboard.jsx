bb3e0bba0f6aa11e6c4246eccea6023c223bfb7c
1Nathane <nathane.miranda@gmail.com>
2022-03-31 18:32:15 -0300

    informações tela entrada

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -5,35 +5,31 @@ import { bindActionCreators } from 'redux'
 import { getSummary } from './dashboardActions'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
-import ValueBox from  '../common/widget/valueBox'
-import Row from  '../common/layout/row'
+import Row from '../common/layout/row'
+import SummaryData from '../batch/SummaryData'
 
 class Dashboard extends Component {
 
     componentWillMount() {
-       this.props.getSummary()
+        this.props.getSummary()
     }
 
     render() {
-        const {expired} = this.props.summary
+        const { expired } = this.props.summary
         return (
-            <div> 
+            <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    <Row> 
-                        <ValueBox cols='12 4' color='green' icon='smile-o'
-                            value={expired} text='Quantidade de lotes com validade acima de 30 dias' />
-                        <ValueBox cols='12 4' color='yellow' icon='exclamation-circle'
-                            value={expired} text='Quantidade de lotes com validade abaixo de 30 dias' />
-                        <ValueBox cols='12 4' color='red' icon='frown-o'
-                            value={expired} text='Quantidade de lotes com validade vendida          ' />
-                    </Row> 
-                </Content> 
+                    <Row>
+
+                        <SummaryData />
+                    </Row>
+                </Content>
             </div>
         )
     }
 }
 
-const mapStateToProps = state => ({summary: state.dashboard.summary})
-const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+const mapStateToProps = state => ({ summary: state.dashboard.summary })
+const mapDispatchToProps = dispatch => bindActionCreators({ getSummary }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
