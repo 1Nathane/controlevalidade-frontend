68891a47a39244d8bd71b4202fc533d05072a158
1Nathane <nathane.miranda@gmail.com>
2022-04-02 01:24:42 -0300

    correção temporaria

--- src/auth/authReducer.js
+++ src/auth/authReducer.js
@@ -15,6 +15,7 @@ export default (state = INITIAL_STATE, action) => {
             }
         case 'USER_FETCHED':
             localStorage.setItem(userKey, JSON.stringify(action.payload))
+            location.reload()
             return { ...state, user: action.payload, validToken: true }
         default:
             return state
