c1b1a83fe32485447288d24c5a9f4ee54cc104a8
1Nathane <nathane.miranda@gmail.com>
2022-05-22 03:56:30 -0300

    url

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
 export default {
-    API_URL: 'https://controlevalidadeapp-backend.herokuapp.com/api',
-    OAPI_URL: 'https://controlevalidadeapp-backend.herokuapp.com/oapi',
+    API_URL: 'https://controlevalidade-backend.herokuapp.com/api',
+    OAPI_URL: 'https://controlevalidade-backend.herokuapp.com/oapi',
 }
